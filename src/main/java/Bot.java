32d806f2d90b24d9ff176065578c6502fcf7417e
Daniel Gonzalez Gregores <dgonzalezgregores@danielcastelao.org>
2022-04-01 09:00:40 +0200

    fusion de los dos bots

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,6 +3,11 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.time.Instant;
 
 public class Bot {
@@ -12,9 +17,14 @@ public class Bot {
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                .title("Loki")
+                .image("attachment:///loki.jpg")
+                .build();
+
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
             final Message message = event.getMessage();
-            if ("hola".equals(message.getContent())) {
+            if ("/embed".equals(message.getContent())) {
 
                 String IMAGE_URL = "https://cdn.betterttv.net/emote/5603401460094fe01db2e3ea/3x";
                 String ANY_URL = "https://youtu.be/br0palt4zg8";
@@ -26,16 +36,30 @@ public class Bot {
                 builder.title("Cangrejo");
                 builder.url(ANY_URL);
                 builder.description("un siri facendo pa");
-                /*builder.addField("file", "inline = true", true);
-                builder.addField("file", "inline = true", true);
-                builder.addField("file", "inline = false", false);*/
                 builder.thumbnail(IMAGE_URL);
                 builder.footer("Hora", IMAGE_URL);
                 builder.timestamp(Instant.now());
                 channel.createMessage(builder.build()).block();
             }
-        });
 
-        gateway.onDisconnect().block();
+        if ("/img".equals(message.getContent())) {
+            final MessageChannel channel = message.getChannel().block();
+
+            InputStream fileAsInputStream = null;
+            try {
+                fileAsInputStream = new FileInputStream("loki.jpg");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            channel.createMessage(MessageCreateSpec.builder()
+                    .content("El dios de las mentiras")
+                    .addFile("loki.jpg", fileAsInputStream)
+                    .addEmbed(embed)
+                    .build()).subscribe();
         }
+    });
+
+            gateway.onDisconnect().block();
     }
+}
